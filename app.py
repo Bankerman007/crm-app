d975eb7c685c48ecb5d17cc7e694537b21e602da
Scott Wright <scottwright.qrf@gmail.com>
2022-12-16 19:21:30 -0600

    database

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 from crm_app import app
-from crm_app import db
-import requests
-import configparser
+
 
 if __name__ =='__main__':
-    # with app.app_context():
-    #     db.create_all()
     app.run(host='0.0.0.0', port=5000)
