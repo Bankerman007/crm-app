80ab85e9bdd585e623f3fe381005c89ed3d94146
Scott Wright <scottwright.qrf@gmail.com>
2022-12-17 10:14:17 -0600

    database

--- crm_app/__init__.py
+++ crm_app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= 'r46ttr29ara'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'#'postgres://yamacrxc:DaRyQOt9c4ZXMIr09J4KvN5RreIU4JsR@raja.db.elephantsql.com/yamacrxc' 
-
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://yamacrxc:DaRyQOt9c4ZXMIr09J4KvN5RreIU4JsR@raja.db.elephantsql.com/yamacrxc' #'sqlite:///site.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+migrate
+
+
 bcrypt=Bcrypt(app)
 login_manager= LoginManager(app)
 login_manager.login_view= 'login'
