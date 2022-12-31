6f50aec390f5c265802c43f94e985b855bf27dfa
Scott Wright <scottwright.qrf@gmail.com>
2022-12-30 23:47:48 -0600

    added api call

--- crm_app/treasury_rate.py
+++ crm_app/treasury_rate.py
@@ -1,51 +1,42 @@
-import pandas as pd
-
+import requests
 
 def rates():
-    scraper = pd.read_html('https://finance.yahoo.com/quote/%5EFVX/')
-    row = pd.concat(scraper)
-    df = pd.DataFrame(row)
-    df_scrub = df.iloc[1:2]
-    rate= df_scrub[1].loc[df_scrub.index[0]]
+    response = requests.get('http://fast-api-rates.justdev.us/rates/1')
+    json_convert= response.json()
+    json_data= json_convert['rate']
+    rate = json_data['rate']
+
     return rate
 
 rates()
 
 def prime_rate():
-    scraper= pd.read_html('https://www.jpmorganchase.com/about/our-business/historical-prime-rate')
-    row = pd.concat(scraper)
-    df = pd.DataFrame(row)
-    df_scrub = df.iloc[0:1]
-    prime = df_scrub.iloc[-1]
-    prime =prime[1]
+    response = requests.get('http://fast-api-rates.justdev.us/rates/2')
+    json_convert= response.json()
+    json_data= json_convert['rate']
+    prime = json_data['rate']
+    
     return prime 
 
 prime_rate()  
 
 def dow():
-    scraper= pd.read_html('https://www.kitco.com/charts/livegold.html')
-    row = pd.concat(scraper)
-    df = pd.DataFrame(row)
-    df_scrub = df.iloc[0:2]
-    dow = df_scrub[1]
-    dow_price = dow[1]
+    response = requests.get('http://fast-api-rates.justdev.us/rates/4')
+    json_convert= response.json()
+    json_data= json_convert['rate']
+    dow_price = json_data['rate']
         
-    #print(dow_price)
-    #print(dow_change)
     return dow_price
 
-def dow_price_change():
-    scraper= pd.read_html('https://www.kitco.com/charts/livegold.html')
-    row = pd.concat(scraper)
-    df = pd.DataFrame(row)
-    df_scrub = df.iloc[0:2]
-    dow = df_scrub[1]
-    dow_change = dow[0]
-    
-    #print(dow_price)
-    #print(dow_change)
-    return dow_change
-    
-
-
 dow()
+
+def dow_price_change():
+    response = requests.get('http://fast-api-rates.justdev.us/rates/3')
+    json_convert= response.json()
+    json_data= json_convert['rate']
+    dow_price_diff = json_data['rate']
+    print(dow_price_diff)
+
+    return dow_price_diff
+
+dow_price_change()
